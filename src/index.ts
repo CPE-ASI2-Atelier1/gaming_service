f4bea5a07e63b0c58e1981438c8b154203960088
arthurjz <arthurjz@hotmail.fr>
2024-12-11 09:21:42 +0100

    init 2

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+// @ts-ignore
+import CONFIG = require('../config.json');
+import {createServer} from "node:http";
+/**
+ *
+ */
+class MyApp {
+    private readonly PORT: number = CONFIG.port;
+    private readonly ADDRESS: string = CONFIG.host;
+    private readonly server:any;
+
+    // ------- Routers -------
+    private application:any;
+
+    constructor() {
+        this.application = express(); // Create application
+        this.server = createServer(this.application); // Create HTTP Server
+
+        //////////////// MIDDLEWARES /////////////////
+        //this.application.use(express.static(CONFIG.www)); // Serve static content
+        this.application.use(express.json()); // ability to parse JSON for POST requests
+        this.application.use((req, res, next) => { // Debugging (prints requests)
+            console.log(`Received ${req.method} request for ${req.url}`);
+            next();
+        });
+
+        ////////////////// ROUTERS ///////////////////
+        // Can be tested through PostMan
+
+        ///////////////// SOCKETS ///////////////////
+        // For now, index.html sends form data to the socket (UserSocket)
+
+        ////////////////// LISTEN ///////////////////
+        // NB : When we use application.listen instead, express creates an internal http server... But it is not accessible
+        // Yet, we need to have access to a http server to use socket.io !
+        this.server.listen(this.PORT, this.ADDRESS, () => {
+            console.log(`Server is running at http://${this.ADDRESS}:${this.PORT}`);
+        });
+    }
+}
+
+const app = new MyApp();
